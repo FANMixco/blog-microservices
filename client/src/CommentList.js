392ca8522acf04eb16674b5a6fbc9bc91f8a7809
Federico Navarrete <fanmixco@gmail.com>
2020-12-13 00:33:53 +0100

    Added moderation service.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,16 @@ import React from 'react';
 export default ({ comments }) => {
 
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'approved')
+      content = comment.content;
+    else if (comment.status === 'pending')
+      content = 'Waiting for moderation';
+    else
+      content = 'Rejected comment';
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
